4686633ca1e7fb23152a42289f91436765dd2ed6
Driitoni <hoti.driton6@gmail.com>
2025-02-23 22:13:53 +0100

    Update global-data.js

--- utils/global-data.js
+++ utils/global-data.js
@@ -7,7 +7,7 @@ export const getGlobalData = () => {
     : '"Vazhdo enderro"';
   const footerText = process.env.BLOG_FOOTER_TEXT
     ? decodeURI(process.env.BLOG_FOOTER_TEXT)
-    : 'All rights reserved.';
+    : '__________________________';
 
   return {
     name,
